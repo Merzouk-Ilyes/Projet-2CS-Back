dc07f2196c6920a9ee021305d2b9b6cd916ceb1d
abir benaissa <abirbenaissa123@gmail.com>
2022-05-17 06:12:10 +0100

    'add_rating'

--- AccountManagement/controllers/notification.js
+++ AccountManagement/controllers/notification.js
@@ -1,4 +1,6 @@
-const Notification = require("../../AccountManagement/models/notification");
+const Notification = require("../models/notification");
+const fetch = require('node-fetch');
+
 exports.addnotification = async (req, res) => {
     const notification = new Notification(req.body);
     await notification.save((err,notification) => {
@@ -9,3 +11,9 @@ exports.addnotification = async (req, res) => {
       }
     });
   };
+exports.test = async (req, res) => {
+
+const response = await fetch('http://localhost:8002/PostHasReservations?idpost=627de24593f2af1a898c4d94');
+const data = await response.json();
+res.json(data);
+}
--- PostManagement/controllers/rating.js
+++ PostManagement/controllers/rating.js
@@ -1,12 +1,18 @@
 const Post = require("../models/Post");
 var mongoose = require('mongoose');
+const fetch = require('node-fetch');
 // THIS FUNCTION MUST BE UPDATED WHEN THE RESERVATION SERVICE IS DONE TO ADD THE CONDITION 
 exports.addrating = async (req, res) => {
 const client= req.query.id;//we should get the value from the rqst
 const id_post = req.query.post;//we should get the value from the rqst
 const ratingvalue= req.body.ratingvalue;//the value from the rating board
 const modifiedPost = Post.findById(id_post);
-Post.updateOne(modifiedPost,{$push: //using push to add a new value without losing the old one 
+fetch('http://localhost:8002/PostHasReservations?idpost='+id_post)
+.then(response => response.json())
+.then((data) => { 
+
+if(data.HasReservations == true){ 
+ Post.updateOne(modifiedPost,{$push: //using push to add a new value without losing the old one 
     {
          rating:{
             ratingValue:ratingvalue,
@@ -17,7 +23,9 @@ Post.updateOne(modifiedPost,{$push: //using push to add a new value without losi
        res.json({msg:"rating added successfully"})//return success msg
    })
    .catch((err)=>{res.send(err);//return err type 
-}) };
+}) 
+}});
+};
 
 
 exports.addcomment = async (req, res) => {
@@ -37,3 +45,12 @@ exports.addcomment = async (req, res) => {
        })
        .catch((err)=>{res.send(err);//return err type 
     }) };
+
+
+exports.test = async (req, res) => {
+
+
+
+    };
+    
+    
