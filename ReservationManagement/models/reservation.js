cc6f5027bbceb695db2226ccaf55be6af8b83d27
med-zino <medzino85@gmail.com>
2022-06-13 01:20:09 +0100

    ad recomndation functions

--- ReservationManagement/models/reservation.js
+++ ReservationManagement/models/reservation.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const User = require("../models/user");
+const mongoose = require('mongoose')
+const User = require('../models/user')
 const reservationSchema = new mongoose.Schema(
   {
     startDate: {
@@ -7,28 +7,28 @@ const reservationSchema = new mongoose.Schema(
       required: true,
     },
     endDate: {
-        type: Date,
-        required: true,
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: Number,
+      required: true,
+      default: 0, //0=WAITING
+    },
+    people: [
+      {
+        age: { type: Number },
+        sexe: { type: Number },
       },
-      status: {
-        type: Number,
-        required: true,
-        default:0//0=WAITING 
-      },
-      people:[
-        {
-          age: { type: Number },
-          sexe: {type:Number},
-        },
-      ],
-      id_user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:User
-      },
-      id_post:{
-        type:mongoose.Schema.Types.ObjectId
-      },
-},
-{ timestamps: true }
-);
-module.exports = mongoose.model("Reservation",  reservationSchema);
+    ],
+    id_user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+    },
+    id_post: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+  },
+  { timestamps: true }
+)
+module.exports = mongoose.model('Reservation', reservationSchema)
